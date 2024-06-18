f5915c342a1ef676909ed606247d04ac1ab4f232
Riku Suzuki <167759438+Riku-tohoku0224@users.noreply.github.com>
2024-06-18 18:07:38 +0900

    Update create_map_madmax.py

--- create_map_madmax.py
+++ create_map_madmax.py
@@ -4,18 +4,44 @@ import rospy
 import ros_numpy
 import message_filters
 from sensor_msgs.msg import Image, PointCloud2
-from geometry_msgs.msg import PoseStamped
+from geometry_msgs.msg import PoseStamped,Point
 import pandas as pd
 from cv_bridge import CvBridge
 import open3d as o3d
 import numpy as np
-from std_msgs.msg import Header
+from std_msgs.msg import Header, ColorRGBA
 import cv2
 from scipy.spatial.transform import Rotation as R
+from bisect import bisect_left
+from visualization_msgs.msg import Marker
 
 tottori_map = o3d.geometry.PointCloud()
 camera_positions = []
 callback_counter = 0
+
+
+def create_marker(points, frame_id):
+    marker = Marker()
+    marker.header.frame_id = frame_id
+    marker.header.stamp = rospy.Time.now()
+    marker.ns = "trajectory"
+    marker.id = 0
+    marker.type = Marker.LINE_STRIP
+    marker.action = Marker.ADD
+    marker.scale.x = 0.05
+    marker.color = ColorRGBA(1.0, 0.0, 0.0, 1.0)
+
+    # Apply transformation matrix for -15 degree rotation around the x-axis
+    for point in points:
+        p = np.array([point[0], point[1], point[2], 1])
+        p_transformed =  p  # Apply the transformation matrix
+        marker_point = Point()
+        marker_point.x = p_transformed[0]
+        marker_point.y = p_transformed[1]
+        marker_point.z = p_transformed[2]
+        marker.points.append(marker_point)
+    
+    return marker
 
 def pose_to_matrix(pose_stamped):
     pose = pose_stamped.pose
@@ -24,11 +50,52 @@ def pose_to_matrix(pose_stamped):
     rotation = R.from_quat(quaternion).as_matrix()
     return rotation, translation
 
-def images_callback(color_img, depth_img, pose_msg, pub, frame_id):
+def pose_generator(csv_file):
+    df = pd.read_csv(csv_file)
+    df.columns = df.columns.str.strip()  # カラム名の前後のスペースを削除
+    poses = []
+    for index, row in df.iterrows():
+        pose = PoseStamped()
+        pose.header = Header()
+        pose.header.stamp = rospy.Time.from_sec(row['% UNIX time'])
+        pose.pose.position.x = row['x(m)']
+        pose.pose.position.y = row['y(m)']
+        pose.pose.position.z = row['z(m)']
+        pose.pose.orientation.w = row['orientation.w']
+        pose.pose.orientation.x = row['orientation.x']
+        pose.pose.orientation.y = row['orientation.y']
+        pose.pose.orientation.z = row['orientation.z']
+        poses.append(pose)
+    return poses
+
+
+def find_closest_pose(poses, target_time):
+    times = [pose.header.stamp.to_sec() for pose in poses]
+    idx = bisect_left(times, target_time.to_sec())
+    if idx == 0:
+        return poses[0]
+    if idx == len(times):
+        return poses[-1]
+    before = times[idx - 1]
+    after = times[idx]
+    if target_time.to_sec() - before < after - target_time.to_sec():
+        return poses[idx - 1]
+    else:
+        return poses[idx]
+
+class PoseStampedSubscriber(message_filters.SimpleFilter):
+    def __init__(self, csv_file):
+        super(PoseStampedSubscriber, self).__init__()
+        self.poses = pose_generator(csv_file)
+        rospy.loginfo(f"PoseStampedSubscriber initialized with file: {csv_file}")
+
+    def find_pose(self, target_time):
+        return find_closest_pose(self.poses, target_time)
+
+def images_callback(color_img, depth_img, pub, pose_subscriber, marker_pub ,frame_id):
     global callback_counter
     callback_counter += 1
 
-    rospy.loginfo("同期した画像とカメラのポーズを受信")
     bridge = CvBridge()
     try:
         header = Header()
@@ -37,30 +104,24 @@ def images_callback(color_img, depth_img, pose_msg, pub, frame_id):
 
         # カラー画像の変換とログ出力
         cv_color_img = bridge.imgmsg_to_cv2(color_img, "bgr8")
-        rospy.loginfo(f"カラー画像の形式: {cv_color_img.dtype}, 形状: {cv_color_img.shape}")
-        
+
         # カラー画像のリサイズを追加
         cv_color_img = cv2.resize(cv_color_img, (1032, 772), interpolation=cv2.INTER_LINEAR)
-        rospy.loginfo(f"リサイズ後のカラー画像の形式: {cv_color_img.dtype}, 形状: {cv_color_img.shape}")
 
         # 深度画像の変換とログ出力
         cv_depth_img = bridge.imgmsg_to_cv2(depth_img, desired_encoding="passthrough")
         cv_depth_img = cv2.resize(cv_depth_img, (1032, 772), interpolation=cv2.INTER_NEAREST)
-        cv_depth_img = np.array(cv_depth_img, dtype=np.float32) * 1000.0
-        rospy.loginfo(f"変換前の深度画像の形式: {cv_depth_img.dtype}, 形状: {cv_depth_img.shape}")
+        cv_depth_img = np.array(cv_depth_img, dtype=np.float32) 
 
         # OpenCVのBGR形式をRGB形式に変換
         cv_color_img = cv2.cvtColor(cv_color_img, cv2.COLOR_BGR2RGB)
-        rospy.loginfo(f"RGB形式に変換後のカラー画像の形式: {cv_color_img.dtype}, 形状: {cv_color_img.shape}")
 
         # OpenCVの画像をOpen3Dの画像形式に変換
         color_raw = o3d.geometry.Image(cv_color_img)
         depth_raw = o3d.geometry.Image(cv_depth_img)
-        rospy.loginfo(f"Open3Dカラー画像の形式: {np.asarray(color_raw).dtype}, 形状: {np.asarray(color_raw).shape}")
-        rospy.loginfo(f"Open3D深度画像の形式: {np.asarray(depth_raw).dtype}, 形状: {np.asarray(depth_raw).shape}")
 
         rgbd_image = o3d.geometry.RGBDImage.create_from_color_and_depth(
-            color_raw, depth_raw, depth_scale=1000.0, depth_trunc=3.0, convert_rgb_to_intensity=False
+            color_raw, depth_raw, depth_scale=1.0, depth_trunc=3.0, convert_rgb_to_intensity=False
         )
 
         intrinsic = o3d.camera.PinholeCameraIntrinsic(
@@ -74,21 +135,62 @@ def images_callback(color_img, depth_img, pose_msg, pub, frame_id):
 
         # RGBD画像から点群を生成
         pcd = o3d.geometry.PointCloud.create_from_rgbd_image(rgbd_image, intrinsic)
-        pcd.transform([[0, 0, 1, 0], [-1, 0, 0, 0], [0, -1, 0, 0], [0, 0, 0, 1]])
+
+        # 点群をNumpy配列に変換
+        points = np.asarray(pcd.points)
+       
+        # ros系からopen3d系に戻す
+        #points = np.dot(R1, points.T).T
+
+        #IMUとカメラの座標系の違いを補正
+        pose = PoseStamped()
+        pose.header = Header()
+        pose.header.stamp = rospy.Time.now()
+        pose.pose.position.x = 0.210
+        pose.pose.position.y = 0
+        pose.pose.position.z = 0.080
+        pose.pose.orientation.x = 0.610
+        pose.pose.orientation.y = -0.604
+        pose.pose.orientation.z =  0.363
+        pose.pose.orientation.w = -0.362
+
+        rotation, translation = pose_to_matrix(pose)
+        points = (np.dot(rotation, points.T).T + translation)
+        
+        # 最も近いポーズデータを取得
+        closest_pose = pose_subscriber.find_pose(color_img.header.stamp)
+        rospy.loginfo(f"Closest PoseStamped: {closest_pose}")
+        
+        # カメラの姿勢の行列を取得
+        T_rotation, T_translation = pose_to_matrix(closest_pose)
+        print(closest_pose)
+        points = (np.dot(T_rotation, points.T).T + T_translation)
+       
+
+
+
+        # 変換された点群をOpen3Dの点群オブジェクトに変換
+        pcd.points = o3d.utility.Vector3dVector(points)
 
         global tottori_map
         global camera_positions
 
         # tottori_mapに点群を追加
         tottori_map += pcd
-        voxel_size = 0.00001
+        voxel_size = 1
         tottori_map = tottori_map.voxel_down_sample(voxel_size=voxel_size)
+
+               # カメラ位置を追加
+        camera_positions.append([closest_pose.pose.position.x,
+                                 closest_pose.pose.position.y - 0.5,
+                                 closest_pose.pose.position.z])
+        marker = create_marker(camera_positions, frame_id)
         
         # ログ出力
         rospy.loginfo(f"ボクセルダウンサンプリング後の点群には {len(tottori_map.points)} 点")
 
-        # 100回ごとにPointCloud2メッセージをpublish
-        if callback_counter % 1 == 0:
+        # 10回ごとにPointCloud2メッセージをpublish
+        if callback_counter % 10 == 0:
             points = np.asarray(tottori_map.points)
             colors = np.asarray(tottori_map.colors) * 255
             colors = colors.astype(np.uint8)
@@ -102,50 +204,26 @@ def images_callback(color_img, depth_img, pose_msg, pub, frame_id):
             pc_array['rgb'] = rgb_colors
 
             pc_msg = ros_numpy.msgify(PointCloud2, pc_array, stamp=header.stamp, frame_id=header.frame_id)
-            pub.publish(pc_msg)
+            #pub.publish(pc_msg)
+            marker_pub.publish(marker)
             rospy.loginfo(f"PointCloud2メッセージを {len(points)} 点で公開")
 
     except Exception as e:
         rospy.logerr(f"処理に失敗: {e}")
 
-def pose_generator(csv_file):
-    df = pd.read_csv(csv_file)
-    for index, row in df.iterrows():
-        pose = PoseStamped()
-        pose.header = Header()
-        pose.header.stamp = rospy.Time.from_sec(row['% UNIX time'])
-        pose.pose.position.x = row[' x(m)']
-        pose.pose.position.y = row[' y(m)']
-        pose.pose.position.z = row[' z(m)']
-        pose.pose.orientation.w = row[' orientation.w']
-        pose.pose.orientation.x = row[' orientation.x']
-        pose.pose.orientation.y = row[' orientation.y']
-        pose.pose.orientation.z = row[' orientation.z']
-        yield pose
-
-class PoseStampedSubscriber(message_filters.SimpleFilter):
-    def __init__(self, csv_file):
-        super(PoseStampedSubscriber, self).__init__()
-        self.pose_gen = pose_generator(csv_file)
-
-    def get(self):
-        try:
-            pose = next(self.pose_gen)
-            self.signalMessage(pose)
-        except StopIteration:
-            rospy.signal_shutdown('No more poses in CSV file')
 
 def listener():
     rospy.init_node('create_dense_map', anonymous=False)
     pub = rospy.Publisher('/point_cloud', PointCloud2, queue_size=10)
+    marker_pub = rospy.Publisher('/camera_trajectory', Marker, queue_size=10)
     frame_id = "camera_left"
 
     color_sub = message_filters.Subscriber("/hcru2/pt_color_rect/left/image", Image)
     depth_sub = message_filters.Subscriber("/hcru2/pt_stereo_sgm/depth", Image)
     pose_sub = PoseStampedSubscriber('/home/riku-suzuki/madmax/B-6_ground_truth/gt_6DoF_gnss_and_imu.csv')
 
-    ts = message_filters.ApproximateTimeSynchronizer([color_sub, depth_sub, pose_sub], 10, 0.1)
-    ts.registerCallback(lambda color, depth, pose: images_callback(color, depth, pose, pub, frame_id))
+    ts = message_filters.ApproximateTimeSynchronizer([color_sub, depth_sub], 10, 0.1)
+    ts.registerCallback(lambda color, depth: images_callback(color, depth, pub, pose_sub, marker_pub ,frame_id))
 
     rospy.spin()
 
@@ -153,4 +231,4 @@ if __name__ == '__main__':
     try:
         listener()
     except rospy.ROSInterruptException:
-        pass
+        rospy.logerr("ROS Interrupt Exception occurred")
